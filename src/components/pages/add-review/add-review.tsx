15b126bfce833f7730e5fcc5281d07637adb2c6f
Mariya-Lisachenko <paramarabara@mail.ru>
2023-11-07 01:43:54 +0500

    Add components and pages

---
+++ src/components/pages/add-review/add-review.tsx
@@ -0,0 +1,51 @@
+import Logo from '../../logo/logo';
+import RatingItemsList from '../../rating/rating';
+import UserBlock from '../../user-block/user-block';
+export default AddReviewPage;
+function AddReviewPage() {
+  return (
+    <body>
+      <section className="film-card film-card--full">
+        <div className="film-card__header">
+          <div className="film-card__bg">
+            <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
+          </div>
+          <h1 className="visually-hidden">WTW</h1>
+          <header className="page-header">
+            <Logo logoClassName="logo__link"/>
+            <nav className="breadcrumbs">
+              <ul className="breadcrumbs__list">
+                <li className="breadcrumbs__item">
+                  <a href="film-page.html" className="breadcrumbs__link">The Grand Budapest Hotel</a>
+                </li>
+                <li className="breadcrumbs__item">
+                  <a className="breadcrumbs__link">Add review</a>
+                </li>
+              </ul>
+            </nav>
+            <UserBlock/>
+          </header>
+          <div className="film-card__poster film-card__poster--small">
+            <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327"/>
+          </div>
+        </div>
+        <div className="add-review">
+          <form action="#" className="add-review__form">
+            <div className="rating">
+              <div className="rating__stars">
+                <RatingItemsList/>
+              </div>
+            </div>
+            <div className="add-review__text">
+              <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text">
+              </textarea>
+              <div className="add-review__submit">
+                <button className="add-review__btn" type="submit">Post</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </body>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const PromoFilmSettings = {
+  promoFilmTitle : 'The Grand Budapest Hotel',
+  promoFilmGenre : 'Drama',
+  promoFilmYear : 2014,
+} as const;
+
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App {...PromoFilmSettings} />
   </React.StrictMode>
 );
