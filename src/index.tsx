2d758ce648fb8c6e2b7a2b07de32044fbbf3a9ca
Mariya-Lisachenko <92264453+Mariya-Lisachenko@users.noreply.github.com>
2023-11-07 05:23:08 +0500

    Revert "Начинаем программировать"

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const PromoFilmSettings = {
-  promoFilmTitle : 'The Grand Budapest Hotel',
-  promoFilmGenre : 'Drama',
-  promoFilmYear : 2014,
-} as const;
-
-
 root.render(
   <React.StrictMode>
-    <App {...PromoFilmSettings} />
+    <h1>Hello, World!</h1>
   </React.StrictMode>
 );
