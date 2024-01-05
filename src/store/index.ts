9aa544e2838596bfc471001b156e62357ac3d28f
Mariya-Lisachenko <paramarabara@mail.ru>
2024-01-05 19:15:27 +0500

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,16 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {stateReducer} from './reducer.ts';
+import {createAPI} from '../api/api';
 
-
-export const store = configureStore({reducer: stateReducer});
+export const api = createAPI();
+export const store = configureStore({
+  reducer: stateReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    })
+});
 
 
